e2d5c53bc8425b9802a2bc6b5c625a63ee3e9ca6
Imaad Uddin <imaaduddin27@gmail.com>
2021-11-03 00:15:37 -0500

    higher order functions

--- functionsAsArguments.js
+++ functionsAsArguments.js
@@ -1,3 +1,42 @@
 // Higher Order Functions 
 
-// Functions that operate on/with
+// Functions that operate on/with other unctions. 
+// They can:
+// - Accept other functions as arguments 
+// - Return a function 
+
+// function callThreeTimes(f) {
+//     f();
+//     f();
+//     f();
+// }
+
+function cry() {
+    console.log("I'm so sad :(");
+}
+
+function rage() {
+    console.log("I'm mad!");
+}
+
+// callThreeTimes(cry);
+
+// function repeatNTimes(action, num) {
+//     for (let i = 0; i < num; i++) {
+//         action();
+//     }
+// }
+
+// repeatNTimes(cry, 10);
+
+function pickOne(f1, f2) {
+    let rand = Math.random();
+    console.log(rand);
+    if (rand < 0.5) {
+        f1();
+    } else {
+        f2();
+    }
+}
+
+pickOne(cry, rage);

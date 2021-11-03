fda8ae6ecde27ee82a8d37fa53fa9c0d21dc63a2
Imaad Uddin <imaaduddin27@gmail.com>
2021-11-03 00:51:00 -0500

    carray callback methods

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+// Hoisting 
+let ninja = "Obito";
+console.log(ninja);
+
+// Always declare variable first before logging or using it
+// With functions you can use it anywhere on top or bottom of declaration 

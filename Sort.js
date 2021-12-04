c21e3913a6c7bb3e1b0f96ba42392a88e1df3fbe
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-04 00:17:58 -0600

    sort method

--- Sort.js
+++ Sort.js
@@ -1,3 +1,9 @@
 // SORT
 
-const prices = [400.50, 3000, 99.99]
+const prices = [400.50, 3000, 99.99, 35.99, 12.00, 9500];
+
+const badSort = prices.slice().sort(); 
+
+const ascSort = prices.slice().sort((a, b) => a - b); // sorting from smallest to largest 
+
+const decSort = prices.slice().sort((a, b) => b - a); // sorting from largest to smallest 

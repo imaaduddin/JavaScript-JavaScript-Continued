51c28721ccaa3ddaa65f17146653abd1a5e64234
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-28 19:00:45 -0600

    methods in objects

--- Deck-Of-Cards.js
+++ Deck-Of-Cards.js
@@ -20,33 +20,57 @@ function drawCard(deck) {
 // const myDeck = makeDeck();
 // const card1 = drawCard(myDeck);
 
-const myDeck = {
-    deck: [],
-    drawnCards: [],
-    suits: ["hearts", "diamonds", "spades", "clubs"],
-    values: "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A",
-    initializeDeck() {
-        const {suits, values, deck} = this;
-        for (let value of values.split(",")) {
-            for (let suit of suits) {
-                deck.push({
-                    value,
-                    suit
-                })
+const makeDeck = () => {
+    return {
+        deck: [],
+        drawnCards: [],
+        suits: ["hearts", "diamonds", "spades", "clubs"],
+        values: "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A",
+        initializeDeck() {
+            const {suits, values, deck} = this;
+            for (let value of values.split(",")) {
+                for (let suit of suits) {
+                    deck.push({
+                        value,
+                        suit
+                    })
+                }
+            }
+            // return deck; 
+        },
+        drawCard() {
+            const card = this.deck.pop();
+            this.drawnCards.push(card);
+            return card;
+        },
+        drawMultiple(numCards) {
+            const cards = [];
+            for (let i = 0; i < numCards; i++) {
+                cards.push(this.drawCard());
+            }
+            return cards;
+        },
+        shuffle() {
+            const {
+                deck
+            } = this;
+            // loop over array backwards
+            for (let i = deck.length - 1; i > 0; i--) {
+                // pick random index before current element
+                let j = Math.floor(Math.random() * (i + 1));
+                // swap
+                [deck[i], deck[j]] = [deck[j], deck[i]];
             }
         }
-        // return deck; 
-    },
-    drawCard() {
-        const card = this.deck.pop();
-        this.drawnCards.push(card);
-        return card;
-    },
-    drawMultiple(numCards) {
-        const cards = [];
-        for (let i = 0; i < numCards; i++) {
-            cards.push(this.drawCard());
-        }
-        return cards;
     }
 }
+
+const myDeck = makeDeck();
+makeDeck.initializeDeck();
+makeDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(3);
+const h3 = myDeck.drawMultiple(5);
+
+const deck2 = makeDeck();
+deck2.initializeDeck();

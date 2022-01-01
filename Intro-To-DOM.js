5d7e8419637d2c2951029e869749dfdc8241d648
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-31 18:41:23 -0600

    DOM

---
+++ Intro-To-DOM.js
@@ -0,0 +1,18 @@
+// What is the DOM
+// The DOM is a JavaScript representation of a webpage
+// It's your JS "window" into the contents of a webpage
+// It's just a bunch of objects that you can interact with via JS
+
+// Document
+// The document object is our entry point into the world of the DOM. It contains representations of all the content on a page, plus tons of useful methods and properties 
+
+
+document.getElementById(); // You can select id tags in HTML file
+
+document.getElementsByTagName(); // You can select the element name like h1 tag or p tag for example
+
+document.getElementsByClassName(); // You can select by the class name in the HTML file
+
+document.querySelector(); // Can replicate the functionality of the methods above, we pass in a CSS selector 
+
+document.querySelectorAll(); // Does same thing as above but returns a collection 

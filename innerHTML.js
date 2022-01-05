a694aecf6ace7a53cc202d35c1c5b9af991fa236
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-04 18:19:20 -0600

    innerHTML

---
+++ innerHTML.js
@@ -0,0 +1,9 @@
+// innerHTML
+
+const h1 = document.querySelector("h1");
+h1.innerHTML = "YOOOO!";
+console.log(h1); // innerHTML can be used to change the element on the html file you selected 
+
+const ul = document.querySelector("ul");
+ul.innerHTML;
+console.log(ul); // Over here innerHTML is displaying everything inside the ul element 
---
+++ innerText-textContent.js
@@ -0,0 +1,9 @@
+// innerText & textContent 
+
+const h1 = document.querySelector("h1");
+h1.innerText;
+console.log(h1); // This will print the h1 value from the html file
+
+const h2 = document.querySelector("h2");
+h2.textContent;
+console.log(h2); // This will print the h2 value from the html file 

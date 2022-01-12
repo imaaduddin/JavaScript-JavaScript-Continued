d59e3fffae705320e46e49259719f8c60817917b
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-11 18:43:38 -0600

    creating elements

---
+++ Creating-Elements.js
@@ -0,0 +1,3 @@
+const newh2 = document.createElement("h2"); // this will create an h2 element 
+
+newh2.innerText = "I like fire!"
---
+++ Manipulating-Classes.js
@@ -0,0 +1,1 @@
+const todo = document.querySelector("#todo .todo"); // This selected all the todo classes inside the todo id

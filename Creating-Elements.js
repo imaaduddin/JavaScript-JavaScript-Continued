f99b69cb5ecef552e6acdfdadd70122422453d50
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-18 17:35:32 -0600

    creating elements

--- Creating-Elements.js
+++ Creating-Elements.js
@@ -1,3 +1,13 @@
 const newh2 = document.createElement("h2"); // this will create an h2 element 
 
-newh2.innerText = "I like fire!"
+newh2.innerText = "I like fire!"; // this will assign the string to h2 created previously
+
+
+
+const newImg = document.createElement("img"); // This will create an image element 
+newImg.src = ""; // You can link any image here and it will display
+
+
+const newLink = document.createElement("a"); // this will create a link tag
+newLink.innerText = "Subscribe! Click Here!"; // This will assign the link to this text
+newLink.href = ""; // This will be link assigned to link you created 

302f21b157e623636da1e835aab0e06af0a23e2d
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-07 16:01:00 -0500

    DOM practice

--- The Impossible Button Demo Project/script.js
+++ The Impossible Button Demo Project/script.js
@@ -1,14 +1,34 @@
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-btn.addEventListener('mouseover', () => {
-    console.log('Moused over me!');
-    const height = Math.floor(Math.random() * window.innerHeight);
-    const width = Math.floor(Math.random() * window.innerWidth);
-    btn.style.left = `${width}px`;
-    btn.style.top = `${height}px`;
-});
+// btn.addEventListener('mouseover', () => {
+//     console.log('Moused over me!');
+//     const height = Math.floor(Math.random() * window.innerHeight);
+//     const width = Math.floor(Math.random() * window.innerWidth);
+//     btn.style.left = `${width}px`;
+//     btn.style.top = `${height}px`;
+// });
 
-btn.addEventListener('click', () => {
-    btn.innerText = 'You got me!';
-    document.body.style.backgroundColor = 'green';
-});
+// btn.addEventListener('click', () => {
+//     btn.innerText = 'You got me!';
+//     document.body.style.backgroundColor = 'green';
+// });
+
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'indigo',
+    'violet'
+];
+
+const container = document.querySelector('#boxes');
+
+for (let color of colors) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = color;
+    box.classList.add('box');
+    container.appendChild(box);
+}

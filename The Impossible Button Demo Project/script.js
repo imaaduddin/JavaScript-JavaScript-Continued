1b14a4c2e9c5c4ca4ce1693ffa3f418266da8e07
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-08 19:57:50 -0500

    addevebtlistener practice

--- The Impossible Button Demo Project/script.js
+++ The Impossible Button Demo Project/script.js
@@ -24,11 +24,20 @@ const colors = [
     'violet'
 ];
 
+// const printColor = () => {
+//     const h1 = document.querySelector('h1');
+//     h1.style.color = this.style.backgroundColor;
+// };
+
 const container = document.querySelector('#boxes');
 
 for (let color of colors) {
     const box = document.createElement('div');
+    const h1 = document.querySelector('h1');
     box.style.backgroundColor = color;
     box.classList.add('box');
     container.appendChild(box);
+    box.addEventListener('click', () => {
+        console.log(h1.style.color = box.style.backgroundColor);
+    })
 }

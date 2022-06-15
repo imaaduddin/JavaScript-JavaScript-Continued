3b6935478b7eaed6a6003ea94ec21f20c5613885
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-14 20:21:36 -0500

    keydown

--- The Impossible Button Demo Project/script.js
+++ The Impossible Button Demo Project/script.js
@@ -13,31 +13,49 @@
 //     document.body.style.backgroundColor = 'green';
 // });
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple',
-    'indigo',
-    'violet'
-];
+// const colors = [
+//     'red',
+//     'orange',
+//     'yellow',
+//     'green',
+//     'blue',
+//     'purple',
+//     'indigo',
+//     'violet'
+// ];
 
 // const printColor = () => {
 //     const h1 = document.querySelector('h1');
 //     h1.style.color = this.style.backgroundColor;
 // };
 
-const container = document.querySelector('#boxes');
+// const container = document.querySelector('#boxes');
 
-for (let color of colors) {
-    const box = document.createElement('div');
-    const h1 = document.querySelector('h1');
-    box.style.backgroundColor = color;
-    box.classList.add('box');
-    container.appendChild(box);
-    box.addEventListener('click', () => {
-        console.log(h1.style.color = box.style.backgroundColor);
-    })
-}
+// for (let color of colors) {
+//     const box = document.createElement('div');
+//     const h1 = document.querySelector('h1');
+//     box.style.backgroundColor = color;
+//     box.classList.add('box');
+//     container.appendChild(box);
+//     box.addEventListener('click', () => {
+//         console.log(h1.style.color = box.style.backgroundColor);
+//     })
+// }
+
+// document.body.addEventListener('keypress', (e) => {
+//     console.log(e);
+// })
+
+const input = document.querySelector('#username');
+
+input.addEventListener('keydown', (e) => {
+    console.log('Key Down!');
+});
+
+input.addEventListener('keyup', (e) => {
+    console.log('Key Up!');
+});
+
+input.addEventListener('keypress', (e) => {
+    console.log('Key Press!');
+});

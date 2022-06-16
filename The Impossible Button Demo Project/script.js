14cd07a9e1acd3f79ff05b078d4cc51c6d685d69
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-15 19:06:16 -0500

    key presses

--- The Impossible Button Demo Project/script.js
+++ The Impossible Button Demo Project/script.js
@@ -59,3 +59,18 @@ input.addEventListener('keyup', (e) => {
 input.addEventListener('keypress', (e) => {
     console.log('Key Press!');
 });
+
+const addItemInput = document.querySelector('#addItem');
+const itemsUL = document.querySelector('#items');
+
+addItemInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        if (!this.value) return;
+        // add a new item to list 
+        const newItemText = this.value; 
+        const newItem = document.createElement('li');
+        newItem.innerText = newItemText;
+        itemsUL.appendChild(newItem);
+        this.value = '';
+    }
+});

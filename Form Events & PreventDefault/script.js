147e9872927ba65c78b60d9eb2a2481fc6b16840
Imaad Uddin <imaaduddin27@gmail.com>
2022-06-27 22:00:00 -0500

    inputs and chnage events

--- Form Events & PreventDefault/script.js
+++ Form Events & PreventDefault/script.js
@@ -1,6 +1,18 @@
 const form = document.querySelector('#signup');
+const creditCard = document.querySelector('#cc');
+const termsCheckbox = document.querySelector('#terms');
+const gadgetSelect = document.querySelector('#gadget');
 
-form.addEventListener('submit', (e) => {
-    alert("Form submitted!");
-    e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//     alert("Form submitted!");
+//     console.log('cc', creditCard.value);
+//     console.log('terms', termsCheckbox.checked);
+//     console.log('gadgets', gadgetSelect.value);
+//     e.preventDefault();
+// });
+
+const formData = {};
+creditCard.addEventListener('input', (e) => {
+    console.log('CC Changed!!!', e);
+    formData['cc'] = e.target.value;
 })

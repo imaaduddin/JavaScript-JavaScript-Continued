0eb9f7472f8e425198f36da61d4004c415d88a5d
Imaad Uddin <imaaduddin27@gmail.com>
2022-07-14 16:16:08 -0500

    forms

--- Form Events & PreventDefault/script.js
+++ Form Events & PreventDefault/script.js
@@ -33,5 +33,6 @@ for (let input of [creditCard, termsCheckbox, gadgetSelect]) {
     input.addEventListener('input', ({target}) => {
         const {name, type, value, checked} = target;
         formData[name] = type === 'checkbox' ? checked : value;
+        console.log(formData);
     });
 }

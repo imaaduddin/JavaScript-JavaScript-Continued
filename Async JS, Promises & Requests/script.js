2565ccacd99fcb61d5b80268f61c9f1782383381
Imaad Uddin <imaaduddin27@gmail.com>
2022-11-14 17:59:48 -0600

    call stacks

---
+++ Async JS, Promises & Requests/script.js
@@ -0,0 +1,26 @@
+const repeat = (str, times) => {
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += str;
+    }
+    return result;
+}
+
+const scream = (str) => {
+    return str.toUpperCase() + '!!!';
+}
+
+const getRantText = (phrase) => {
+    let text = scream(phrase);
+    let rant = repeat(text, 8);
+    return rant;
+}
+
+const makeRant = (phrase, el) => {
+    const h1 = document.getElement('h1');
+    h1.innerText = getRantText(phrase);
+    el.appendChild(h1);
+}
+
+makeRant('I dislike oily foods', document.body);
+makeRant('This guy is always camping in the game!', document.body);
